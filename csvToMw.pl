298ed99a0239e01557c15aa38fc820cbd3c7550b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-08-26 08:07:06 +0200

    Add csvToMw.pl

---
+++ csvToMw.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+#Retrieved from "http://wiki.crc.nd.edu/wiki/index.php/Convert_CSV_To_Wiki_Table"
+open(INPUT, "< input.csv")
+       or die "Couldn't open file for reading: $!\n";
+open(OUTPUT, "> output.txt")
+       or die "Couldn't open file for writing: $!\n";   
+
+while (<INPUT>) {
+       @fields = split(",");
+       printf OUTPUT ("\|$fields[0]\n\|$fields[1]\n\|$fields[2]\n\|$fields[3]\|-\n");
+}
+close INPUT;
+close OUTPUT;
