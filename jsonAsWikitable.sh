b6c48beb754bd7b130070e107b5df58df61a2064
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-08-26 10:28:24 +0200

    Wget &> /dev/null + copy current res to the public space

--- jsonAsWikitable.sh
+++ jsonAsWikitable.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget -O input.csv https://json-csv.com/?u=http://tools.wmflabs.org/urbanecmbot/test/wlmMonitoringOutput.json
+cp output.json /data/project/urbanecmbot/test/wlmMonitoringOutput.json
+wget -O input.csv https://json-csv.com/?u=http://tools.wmflabs.org/urbanecmbot/test/wlmMonitoringOutput.json &> /dev/null
 ./csvToMw.pl
 rm input.csv
